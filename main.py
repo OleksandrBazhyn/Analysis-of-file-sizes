04bbacb4a9990e0a0ba3fd67cb424d80f1d638b2
Oleksandr Bazhyn <olexandrbazhyn@gmail.com>
2025-03-23 18:44:25 +0000

    Change dispasons

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ if sizes.size == 0:
     print("Файл `gathered_file_sizes.txt` порожній або містить некоректні дані.")
     exit()
 
-# Нові діапазони розмірів (додано "<0.5 KB")
-bins = [0, 512, 1024, 10_240, 100_240, 1_024_000, 10_240_000, 100_240_000, 
-        1_024_000_000, 10_240_000_000, float("inf")]
-labels = ["0 B - 0.5 KB", "0.5 KB - 1 KB", "1 KB - 10 KB", "10 KB - 100 KB", 
-          "100 KB - 1 MB", "1 MB - 10 MB", "10 MB - 100 MB", 
-          "100 MB - 1 GB", "1 GB - 10 GB", "10 GB+"]
+# Нові діапазони розмірів (додано між 1 КБ і 5 КБ, і прибрано більше 10 Гб)
+bins = [0, 512, 1024, 5_120, 10_240, 100_240, 1_024_000, 10_240_000, 100_240_000, 
+        1_024_000_000, float("inf")]
+labels = ["0 B - 0.5 KB", "0.5 KB - 1 KB", "1 KB - 5 KB", "5 KB - 10 KB", 
+          "10 KB - 100 KB", "100 KB - 1 MB", "1 MB - 10 MB", "10 MB - 100 MB", 
+          "100 MB - 1 GB", "1 GB+"]
 
 # Розподіл файлів по групах
 hist, _ = np.histogram(sizes, bins=bins)
@@ -29,7 +29,7 @@ total_files = sizes.size
 percentages = (hist / total_files) * 100  # Відсоткове співвідношення
 
 # Побудова гістограми
-plt.figure(figsize=(16, 9))
+plt.figure(figsize=(12, 6))
 bars = plt.bar(labels, hist, color="skyblue", edgecolor="black", alpha=0.7)
 
 # Додавання підписів із відсотками та кількістю файлів
